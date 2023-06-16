3a6eec16a0d42e535881bd65a1f1741cff5dad7a
Tanveer Hossain Jony <thjbd19@gmail.com>
2023-06-16 16:08:12 +0600

    added drawer

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -82,6 +82,30 @@ const Home = () => {
           ))}
         </div>
       </div>
+
+      {/* drawer  */}
+
+      <div className="drawer">
+        <input id="my-drawer" type="checkbox" className="drawer-toggle" />
+        <div className="drawer-content mx-auto m-2">
+          {/* Page content here */}
+          <label htmlFor="my-drawer" className="btn btn-info drawer-button">
+            Open drawer
+          </label>
+        </div>
+        <div className="drawer-side">
+          <label htmlFor="my-drawer" className="drawer-overlay"></label>
+          <ul className="menu p-4 w-80 h-full bg-base-200 text-base-content">
+            {/* Sidebar content here */}
+            <li>
+              <a>Sidebar Item 1</a>
+            </li>
+            <li>
+              <a>Sidebar Item 2</a>
+            </li>
+          </ul>
+        </div>
+      </div>
     </div>
   );
 };
