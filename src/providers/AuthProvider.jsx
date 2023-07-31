bc1e0a0bd6cda730871d1565bb33ae014469f9d9
Tanveer Hossain Jony <thjbd19@gmail.com>
2023-07-31 23:31:31 +0600

    78-7 (Recap) Create a JWT token and save it on client side

--- src/providers/AuthProvider.jsx
+++ src/providers/AuthProvider.jsx
@@ -8,6 +8,7 @@ import {
   updateProfile,
 } from "firebase/auth";
 import app from "../firebase/firebase.config";
+import axios from "axios";
 
 export const AuthContext = createContext();
 const auth = getAuth(app);
@@ -44,13 +45,26 @@ const AuthProvider = ({ children }) => {
   useEffect(() => {
     const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
       setUser(currentUser);
-      // console.log("current user", currentUser);
+      console.log("current user", currentUser);
+
+      // get and set token
+      if (currentUser) {
+        axios
+          .post("http://localhost:5000/jwt", { email: currentUser.email })
+          .then((data) => {
+            // console.log(data.data.token);
+            localStorage.setItem("access-token", data.data.token);
+          });
+      } else {
+        localStorage.removeItem("access-token");
+      }
+
       setLoading(false);
     });
     return () => {
       return unsubscribe();
     };
-  });
+  }, []);
 
   const authInfo = {
     user,
