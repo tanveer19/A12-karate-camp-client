13975497c427701ec574f088f09a71982e053da5
Tanveer Hossain Jony <thjbd19@gmail.com>
2023-08-04 12:12:40 +0600

    80-4 Use Test card and display card error

---
+++ src/pages/Dashboard/Payment/_payment_integration_steps.js
@@ -0,0 +1,7 @@
+/*
+
+1. install stripe and react stripe js
+2. create a checkout form with card element (card element contains : card number, expiration date, cvs, zip code)
+3. create account on stripe and get publishable key pk
+4. 
+*/
