4b4d361377cb30f05f2f04b008d309a4750fa58b
Tanveer Hossain Jony <thjbd19@gmail.com>
2023-08-04 17:39:39 +0600

    80-5 Server side payment intents API to get client Secret from stripe

--- src/pages/Dashboard/Payment/_payment_integration_steps.js
+++ src/pages/Dashboard/Payment/_payment_integration_steps.js
@@ -3,5 +3,12 @@
 1. install stripe and react stripe js
 2. create a checkout form with card element (card element contains : card number, expiration date, cvs, zip code)
 3. create account on stripe and get publishable key pk
-4. 
+4. get card info
+5. create a payment method
+6. use test card to test payment
+7. on the server side: install stripe:
+yarn add stripe
+8. create a payment intent api with payment method types: ['card]
+9. provide amount in cents (multiply price with 100)
+
 */
