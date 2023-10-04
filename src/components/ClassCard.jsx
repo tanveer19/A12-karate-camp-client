40f92c51fd200fe34c191ce34d13d6a9949c5b7d
Tanveer Hossain Jony <thjbd19@gmail.com>
2023-10-04 19:02:35 +0600

    77-2 API naming convention and API to save Cart

--- src/components/ClassCard.jsx
+++ src/components/ClassCard.jsx
@@ -1,7 +1,44 @@
-import React from "react";
+import { useContext } from "react";
+import { AuthContext } from "../providers/AuthProvider";
+import Swal from "sweetalert2";
+import { useNavigate } from "react-router-dom";
 
 const ClassCard = ({ item }) => {
-  const { name, image, price, category } = item;
+  const { name, image, price } = item;
+  const { user } = useContext(AuthContext);
+  const navigate = useNavigate();
+
+  const handleAddToCart = (item) => {
+    console.log(item);
+    if (user) {
+      fetch("https://2-21-a12-summer-camp-server.vercel.app/carts")
+        .then((res) => res.json())
+        .then((data) => {
+          if (data.insertedId) {
+            Swal.fire({
+              position: "top-end",
+              icon: "success",
+              title: "class added on the cart.",
+              showConfirmButton: false,
+              timer: 1500,
+            });
+          }
+        });
+    } else {
+      Swal.fire({
+        title: "Please login to order class?",
+        icon: "warning",
+        showCancelButton: true,
+        confirmButtonColor: "#3085d6",
+        cancelButtonColor: "#d33",
+        confirmButtonText: "Login now",
+      }).then((result) => {
+        if (result.isConfirmed) {
+          navigate("/login");
+        }
+      });
+    }
+  };
   return (
     <div>
       <div className="card card-compact w-96 bg-base-100 shadow-xl">
@@ -15,7 +52,10 @@ const ClassCard = ({ item }) => {
           <h2 className="card-title">Class Name: {name}</h2>
           <p>Description: {}</p>
           <div className="card-actions justify-end">
-            <button className="btn btn-outline border-l-2 border-r-2 border-0">
+            <button
+              onClick={() => handleAddToCart(item)}
+              className="btn btn-outline border-l-2 border-r-2 border-0"
+            >
               Add to Cart
             </button>
           </div>
@@ -24,5 +64,4 @@ const ClassCard = ({ item }) => {
     </div>
   );
 };
-
 export default ClassCard;
