573efde1f6f60382ddce718757eaedf44696a2ca
Tanveer Hossain Jony <thjbd19@gmail.com>
2023-10-05 17:05:00 +0600

    77-4 Simple Overview of React Query and load data

--- src/hooks/useCart.jsx
+++ src/hooks/useCart.jsx
@@ -1,29 +1,20 @@
 import { useQuery } from "@tanstack/react-query";
-import useAxiosSecure from "./useAxiosSecure";
-import useAuth from "./useAuth";
+import { useContext } from "react";
+import { AuthContext } from "../providers/AuthProvider";
 
 const useCart = () => {
-  const { user, loading } = useAuth();
-  // const token = localStorage.getItem('access-token');
-  const [axiosSecure] = useAxiosSecure();
+  const { user } = useContext(AuthContext);
 
   const { refetch, data: cart = [] } = useQuery({
     queryKey: ["carts", user?.email],
-    enabled: !loading,
-    // queryFn: async () => {
-    //   const res = await axiosSecure(`/carts?email=${user?.email}`);
-    //   console.log("res from axios", res);
-    //   return res.data;
-    // },
+    queryFn: async () => {
+      const res = await fetch(
+        `https://2-21-a12-summer-camp-server.vercel.app/carts?email=${user?.email}`
+      );
+      return res.json();
+    },
   });
-
   return [cart, refetch];
 };
-export default useCart;
 
-// queryFn: async () => {
-//     const res = await fetch(`https://2-21-a12-summer-camp-server.vercel.app/carts?email=${user?.email}`, { headers: {
-//         authorization: `bearer ${token}`
-//     }})
-//     return res.json();
-// },
+export default useCart;
