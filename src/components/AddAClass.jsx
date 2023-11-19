4296ef39ffdfde2f6c29273383ea078b2d453b17
Tanveer Hossain Jony <thjbd19@gmail.com>
2023-11-19 12:24:56 +0600

    79-5 Upload image to image hosting server imgbb and get image url

--- src/components/AddAClass.jsx
+++ src/components/AddAClass.jsx
@@ -4,36 +4,52 @@ import { useContext } from "react";
 import { AuthContext } from "../providers/AuthProvider";
 import Swal from "sweetalert2";
 
+const img_hosting_token = import.meta.env.VITE_Image_Upload_token;
+
 const AddAClass = () => {
   const { user } = useContext(AuthContext);
-
   const {
     register,
     handleSubmit,
-    watch,
     formState: { errors },
   } = useForm();
+  const img_hosting_url = `https://api.imgbb.com/1/upload?expiration=600&key=${img_hosting_token}`;
+
   const onSubmit = (data) => {
-    fetch("http://localhost:5000/postClasses", {
+    // fetch("http://localhost:5000/postClasses", {
+    //   method: "POST",
+    //   headers: { "Content-Type": "application/json" },
+    //   body: JSON.stringify(data),
+    // })
+    //   .then((res) => res.json())
+    //   .then((result) => {
+    //     console.log(result);
+
+    //     Swal.fire({
+    //       position: "top-end",
+    //       icon: "success",
+    //       title: "Class Added successfully.",
+    //       showConfirmButton: false,
+    //       timer: 1500,
+    //     });
+    //   });
+
+    const formData = new FormData();
+    formData.append("image", data.image[0]);
+
+    fetch(img_hosting_url, {
       method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify(data),
+      body: formData,
     })
       .then((res) => res.json())
-      .then((result) => {
-        console.log(result);
-
-        Swal.fire({
-          position: "top-end",
-          icon: "success",
-          title: "Class Added successfully.",
-          showConfirmButton: false,
-          timer: 1500,
-        });
-      });
-
-    console.log(data);
+      .then((imgResponse) => {
+        console.log(imgResponse);
+      })
   };
+
+  console.log(errors);
+  console.log(img_hosting_token);
+
   return (
     <div>
       <Helmet>
@@ -62,12 +78,11 @@ const AddAClass = () => {
           /> */}
 
           <select
+            defaultValue="Pick one"
             {...register("category", { required: true })}
             className="select select-bordered"
           >
-            <option selected disabled>
-              Pick Class Category
-            </option>
+            <option disabled>Pick one</option>
             <option value="japan">Japan</option>
             <option value="korea">Korea</option>
             <option value="china">China</option>
