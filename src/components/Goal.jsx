8d92b3f8bed879c874645ab36eaf12005719d411
Tanveer Hossain Jony <thjbd19@gmail.com>
2024-03-29 20:02:01 +0600

    added transition, fixed mobile resp. for goal section

--- src/components/Goal.jsx
+++ src/components/Goal.jsx
@@ -2,16 +2,16 @@ import React from "react";
 
 const Goal = () => {
   return (
-    <div className="h-screen font-sans font-bold bg-gray-700">
-      <section className="mt-36 flex justify-center">
+    <div className="md:h-screen font-sans font-bold bg-gray-700">
+      <section className="mt-36 flex justify-center text-center">
         <h1 className="mt-36 text-6xl text-white">WHAT IS YOUR TARGET?</h1>
       </section>
       <section class="text-gray-600 body-font ">
         <div class="container px-5 py-24 mx-auto flex flex-wrap">
           <div class="lg:w-full mx-auto ">
-            <div class="flex flex-wrap -mx-2">
+            <div class="flex flex-wrap gap-5 md:gap-0 -mx-2 transition-all duration-1000 grayscale hover:grayscale-0 focus:grayscale-0 active:grayscale-0">
               <div class="px-2 w-full md:w-1/3">
-                <div class="flex flex-wrap w-full sm:py-24 py-16 sm:px-10 px-6 relative grayscale hover:grayscale-0">
+                <div class="flex flex-wrap w-full sm:py-24 py-16 sm:px-10 px-6 relative ">
                   <img
                     alt="gallery"
                     class="w-full object-cover h-full object-center block  absolute inset-0 rounded-3xl"
@@ -31,7 +31,7 @@ const Goal = () => {
                 </div>
               </div>
               <div class="px-2  w-full md:w-1/3">
-                <div class="flex flex-wrap w-full sm:py-24 py-16 sm:px-10 px-6 relative grayscale hover:grayscale-0">
+                <div class="flex flex-wrap w-full sm:py-24 py-16 sm:px-10 px-6 relative transition-all duration-1000 grayscale hover:grayscale-0 focus:grayscale-0 active:grayscale-0">
                   <img
                     alt="gallery"
                     class="w-full object-cover h-full object-center block  absolute inset-0 rounded-3xl"
@@ -51,7 +51,7 @@ const Goal = () => {
                 </div>
               </div>
               <div class="px-2  w-full md:w-1/3">
-                <div class="flex flex-wrap w-full sm:py-24 py-16 sm:px-10 px-6 relative grayscale hover:grayscale-0">
+                <div class="flex flex-wrap w-full sm:py-24 py-16 sm:px-10 px-6 relative transition-all duration-1000 grayscale hover:grayscale-0 focus:grayscale-0 active:grayscale-0">
                   <img
                     alt="gallery"
                     class="w-full object-cover h-full object-center block  absolute inset-0 rounded-3xl"
